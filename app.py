e337fd4b1fa271adc672b893eaa750c8d13026b6
rorypearson26 <rorypearson2605@gmail.com>
2022-01-08 23:48:59 +0000

    Basis app functionality...not working.

---
+++ app.py
@@ -0,0 +1,71 @@
+import os
+
+from decouple import config
+from slack_bolt import App
+
+BOT_TOKEN = config("SLACK_BOT_TOKEN")
+APP_TOKEN = config("SLACK_APP_TOKEN")
+SIGNING_SECRET = config("SLACK_SIGNING_SECRET")
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=TOKEN,
+    signing_secret=SIGNING_SECRET
+)
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
+
+# Start your app
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    print(f"PORT IS {port}")
+    app.start(port=port)
