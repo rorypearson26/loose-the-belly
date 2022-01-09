25b18030adbc99a519324415d35467e13d2f1086
rorypearson26 <rorypearson2605@gmail.com>
2022-01-09 22:45:34 +0000

    Still playing but have a least connected.

--- app.py
+++ app.py
@@ -2,70 +2,29 @@ import os
 
 from decouple import config
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 BOT_TOKEN = config("SLACK_BOT_TOKEN")
 APP_TOKEN = config("SLACK_APP_TOKEN")
 SIGNING_SECRET = config("SLACK_SIGNING_SECRET")
 
 # Initializes your app with your bot token and signing secret
-app = App(
-    token=TOKEN,
-    signing_secret=SIGNING_SECRET
-)
+app = App(token=BOT_TOKEN, signing_secret=SIGNING_SECRET)
+app.client.apps_connections_open(app_token=APP_TOKEN)
 
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-  try:
-    # views.publish is the method that your app uses to push a view to the Home tab
-    client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
 
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
+# Listens to incoming messages that contain "hello"
+@app.event("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    app.client.chat_postMessage(
+        channel="#weight-tracker", text=f"Hey there <@{message['user']}>!"
     )
-
-  except Exception as e:
-    logger.error(f"Error publishing home tab: {e}")
 
 
 # Start your app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
-    print(f"PORT IS {port}")
     app.start(port=port)
+    # handler = SocketModeHandler(app, APP_TOKEN)
+    # handler.start()
