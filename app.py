7f100d4ed359192b8170363e857410f7445bedb4
rorypearson26 <rorypearson2605@gmail.com>
2022-01-10 22:49:05 +0000

    Finally got the thing hooked in so can start adding functionality.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import os
+"""Module containing routing for messages coming from slack.
 
+
+"""
 from decouple import config
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -8,23 +10,19 @@ BOT_TOKEN = config("SLACK_BOT_TOKEN")
 APP_TOKEN = config("SLACK_APP_TOKEN")
 SIGNING_SECRET = config("SLACK_SIGNING_SECRET")
 
-# Initializes your app with your bot token and signing secret
-app = App(token=BOT_TOKEN, signing_secret=SIGNING_SECRET)
-app.client.apps_connections_open(app_token=APP_TOKEN)
+app = App(token=BOT_TOKEN)
 
 
-# Listens to incoming messages that contain "hello"
-@app.event("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    app.client.chat_postMessage(
-        channel="#weight-tracker", text=f"Hey there <@{message['user']}>!"
-    )
+@app.message("add")
+def add_message(message, say):
+
+    say(f"Hey there <@{message['user']}>!")
 
 
-# Start your app
+def main():
+    handler = SocketModeHandler(app, APP_TOKEN)
+    handler.start()
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    app.start(port=port)
-    # handler = SocketModeHandler(app, APP_TOKEN)
-    # handler.start()
+    main()
