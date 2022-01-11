fad74f716f4357331bbe458e25fd4e7fe2bf38f7
rorypearson26 <rorypearson2605@gmail.com>
2022-01-11 21:03:13 +0000

    Added mop up function to catch unmatched messages.

--- app/app.py
+++ app/app.py
@@ -2,11 +2,13 @@
 
 
 """
+import re
+
 from decouple import config
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-from helper_functions import get_weight
+from utilities.helper_functions import get_weight
 
 BOT_TOKEN = config("SLACK_BOT_TOKEN")
 APP_TOKEN = config("SLACK_APP_TOKEN")
@@ -15,7 +17,14 @@ SIGNING_SECRET = config("SLACK_SIGNING_SECRET")
 app = App(token=BOT_TOKEN)
 
 
-@app.message("add")
+@app.message(re.compile(r".*", flags=re.IGNORECASE))
+def mop_up_message(message, say):
+    """Listens for messages containing `add` so that weights can be added."""
+    message_str = message["text"]
+    say(f"Listened to: `{message_str}` but did not invoke any actions.")
+
+
+@app.message(re.compile(r"(?<=add )(\d+.\d+|\d+)", flags=re.IGNORECASE))
 def add_message(message, say):
     """Listens for messages containing `add` so that weights can be added."""
     weight = get_weight(message_str=message["text"])
