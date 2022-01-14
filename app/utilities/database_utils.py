210ab67a0ff58d74885772c2bf903a79b58567ae
rorypearson26 <rorypearson2605@gmail.com>
2022-01-14 22:47:40 +0000

    Added session and add command.

--- app/utilities/database_utils.py
+++ app/utilities/database_utils.py
@@ -1,47 +1,52 @@
 """Module to keep all database functionality in one place."""
-import sqlite3
+from datetime import datetime
+import time
 
-from sqlalchemy import (Column, DateTime, Float, Integer, MetaData, String,
-                        Table, create_engine, engine)
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Float,
+    Integer,
+    String,
+    create_engine,
+    engine,
+)
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 Base = declarative_base()
 DATABASE_NAME = "app/weights.db"
-initialise_database()
+engine = create_engine(f"sqlite:///{DATABASE_NAME}")
+Session = sessionmaker(engine)
 
-class Weight(Base):
-    __tablename__ = 'weights'
-    id = Column(Integer, primary_key=True),
-    name = Column(String),
-    weight = Column(Float),
-    date = Column(DateTime),
-    clothing_code = Column(String),
-    
+
 def initialise_database():
-    meta = MetaData()
-    engine = create_engine(f"sqlite:///{DATABASE_NAME}")
     if not database_exists(engine.url):
         create_database(engine.url)
-        weights = Table(
-            "weights",
-            meta,
-            Column("id", Integer, primary_key=True),
-            Column("name", String),
-            Column("weight", Float),
-            Column("date", DateTime),
-            Column("clothing_code", String)
-        )
-        meta.create_all(engine)
-    return engine
+        time.sleep(3)
+        Weight.__table__.create(engine)
 
 
-# def populate_from_csv():
+class Weight(Base):
+    __tablename__ = "weights"
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    weight = Column(Float)
+    date = Column(DateTime, default=datetime.now())
+    clothing_code = Column(String, default="l")
 
 
-def add_weight_measurement(weight):
-    with engine
+def add_measurement(weight, clothing_code, date=datetime.now()):
+    """Adds weight measurement coming from the slack app to `Weight` table.
 
-
-# if __name__ == "__main__":
-#     create_database()
+    Args:
+        weight (float): [description]
+        clothing_code (str) [description]
+        date (datetime, optional): Date measurement was recorded. Defaults to current datetime.
+    """
+    measurement = Weight(weight=weight, date=date, clothing_code=clothing_code)
+    with Session() as s:
+        s.add(measurement)
+        s.commit()
