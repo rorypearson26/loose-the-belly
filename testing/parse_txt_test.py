928355e172c59ba90cfd411f21a84eccaff3dd17
rorypearson26 <rorypearson2605@gmail.com>
2022-01-24 22:41:52 +0000

    Got mocks working as should...need to look at regexs.

--- testing/parse_txt_test.py
+++ testing/parse_txt_test.py
@@ -17,6 +17,14 @@ class ParseTxtTestModule(unittest.TestCase):
         self.clothing_code_regex = self.retrieve_regex("clothing_code")
 
     @staticmethod
+    def set_up_mock(regex, input_text, cast_to_type=str):
+        mock = MagicMock()
+        mock.regex = regex
+        mock.input_text = input_text
+        mock.cast_to_type = cast_to_type
+        return mock
+
+    @staticmethod
     def retrieve_regex(regex_name):
         mock = MagicMock()
         mock.regex_name = regex_name
@@ -24,60 +32,59 @@ class ParseTxtTestModule(unittest.TestCase):
         return regex
 
     def test_returns_false_when_not_matched_using_date_regex(self):
-        print(self.weight_regex)
-        # input_strings = [
-        #     "No date in format %d-%m-%y here",
-        #     "20-1-19",
-        #     "1-1-2000",
-        #     "20/11/21",
-        # ]
-        # self.false_test(input_strings, "date")
+        input_strings = [
+            "No date in format %d-%m-%y here",
+            "20-1-19",
+            "1-1-2000",
+            "20/11/21",
+        ]
+        self.false_test(input_strings, self.date_regex)
 
-    # def test_returns_false_when_not_matched_using_clothing_code_regex(self):
-    #     input_strings = ["no clothing code here", "nhl hl nh", "n ", "h", "l"]
-    #     self.false_test(input_strings, "clothing_code")
+    def test_returns_false_when_not_matched_using_clothing_code_regex(self):
+        input_strings = ["no clothing code here", "nhl hl nh", "n ", "h", "l"]
+        self.false_test(input_strings, self.clothing_code_regex)
 
-    # def test_returns_false_when_not_matched_using_weight_regex(self):
-    #     input_strings = ["100.0fs", "not a number", "80df"]
-    #     self.false_test(input_strings, "weight")
+    def test_returns_false_when_not_matched_using_weight_regex(self):
+        input_strings = ["100.0fs", "not a number", "80df"]
+        self.false_test(input_strings, self.weight_regex)
 
-    # def test_returns_expected_match_using_weight_regex(self):
-    #     input_output_dict = {
-    #         "11": "add 11 this",
-    #         "12.4": "add 12.4",
-    #         "13": "add 13",
-    #         "14.5": "add 14.5 n",
-    #     }
-    #     self.true_test(input_output_dict, "weight")
+    def test_returns_expected_match_using_weight_regex(self):
+        input_output_dict = {
+            "11": "add 11 this",
+            "12.4": "add 12.4",
+            "13": "add 13",
+            "14.5": "add 14.5 n",
+        }
+        self.true_test(input_output_dict, self.weight_regex)
 
-    # def test_returns_expected_match_using_date_regex(self):
-    #     input_output_dict = {
-    #         "11-12-22": "add 11 this 11-12-22",
-    #         "12-12-22": "add 11 this 12-12-22 n",
-    #     }
-    #     self.true_test(input_output_dict, "date")
+    def test_returns_expected_match_using_date_regex(self):
+        input_output_dict = {
+            "11-12-22": "add 11 this 11-12-22",
+            "12-12-22": "add 11 this 12-12-22 n",
+        }
+        self.true_test(input_output_dict, self.date_regex)
 
-    # def test_returns_expected_match_using_clothing_code_regex(self):
-    #     input_output_dict = {
-    #         "n": "add 11 11-12-22 n",
-    #         "l": "add 11 12-12-22 l",
-    #         "h": "add 11 12-12-22 h",
-    #     }
-    #     self.true_test(input_output_dict, "clothing_code")
+    def test_returns_expected_match_using_clothing_code_regex(self):
+        input_output_dict = {
+            "n": "add 11 11-12-22 n ",
+            "l": "add 11 12-12-22 l ",
+            "h": "add 11 12-12-22 h ",
+        }
+        self.true_test(input_output_dict, self.clothing_code_regex)
 
-    # def false_test(self, input_strings, regex_type):
-    #     regex = self.regex_string_dict[regex_type]
-    #     for test in input_strings:
-    #         func_result = parse_txt(test, regex)
-    #         with self.subTest(f"{test} should return `False`"):
-    #             self.assertFalse(func_result)
+    def false_test(self, input_strings, regex):
+        for test_str in input_strings:
+            mock = self.set_up_mock(regex=regex, input_text=test_str)
+            func_result = TextParser.parse_txt(mock)
+            with self.subTest(f"{test_str} should return `False`"):
+                self.assertFalse(func_result)
 
-    # def true_test(self, input_output_dict, regex_type):
-    #     regex = self.regex_string_dict[regex_type]
-    #     for expected, input_str in input_output_dict.items():
-    #         func_result = parse_txt(input_str, regex)
-    #         with self.subTest(f"`{input_str}` should return `{expected}`"):
-    #             self.assertEqual(expected, func_result)
+    def true_test(self, input_output_dict, regex):
+        for expected, test_str in input_output_dict.items():
+            mock = self.set_up_mock(regex=regex, input_text=test_str)
+            func_result = TextParser.parse_txt(mock)
+            with self.subTest(f"`{test_str}` should return `{expected}`"):
+                self.assertEqual(expected, func_result)
 
 
 if __name__ == "__main__":
